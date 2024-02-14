dd04415ec67dfafd660ed45d06a60784be2a2d14
Sasen Tamrakar <sasen.tamrakar@yajtechnologies.com>
2024-02-15 00:00:39 +0545

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:connect_me/auth/presentation/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
